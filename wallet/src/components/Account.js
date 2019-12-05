fe3acaa9dcf5a68395f019304abeb24ebcf3c9a4
DaftBeowulf <ajmboosh@gmail.com>
2019-12-05 16:44:29 -0600

    :sparkles: MVP complete, basic React app shows user data
    User can enter or edit their id, then will show current account balance of lambda coins and each transaction involving their id

--- wallet/src/components/Account.js
+++ wallet/src/components/Account.js
@@ -1,17 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const port = "http://localhost:5000";
 
 const Account = ({ walletId }) => {
+  const [userTxns, setUserTxns] = useState([]);
+  const [currency, setCurrency] = useState(0);
   useEffect(() => {
     const url = port + "/chain";
-    axios.get(url).then(res => {
-      console.log(res.data.chain);
-    });
-  });
+    axios
+      .get(url)
+      .then(res => {
+        const chain = res.data.chain;
+        const temp_txns = [];
+        let temp_currency = 0;
+        chain.forEach(block => {
+          block.transactions.forEach(txn => {
+            if (txn.sender === walletId) {
+              temp_currency -= txn.amount;
+              temp_txns.push(txn);
+            } else if (txn.recipient === walletId) {
+              temp_currency += txn.amount;
+              temp_txns.push(txn);
+            }
+          });
+        });
+        setCurrency(temp_currency);
+        setUserTxns(temp_txns);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [walletId]);
 
-  return <div className="account">Yoooo</div>;
+  return (
+    <div className="account">
+      <h1>Current account balance: {currency} LambdaCoins</h1>
+      {userTxns.length ? (
+        <div>
+          {userTxns.map(txn => (
+            <div>
+              Recipient: {txn.recipient} Sender: {txn.sender} Amount:{" "}
+              {txn.amount}
+            </div>
+          ))}
+        </div>
+      ) : walletId ? (
+        <div>No transactions under your ID yet, {walletId}</div>
+      ) : (
+        <div>Enter an ID above to see your transactions!</div>
+      )}
+    </div>
+  );
 };
 
 export default Account;
